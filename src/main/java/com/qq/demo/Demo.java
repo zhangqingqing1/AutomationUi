169ef3f7aa7370038e6b3f9494a3946c99210d4e
鲁奇伟 <qiwei.lu@b-and-qchina.com>
2019-11-26 15:15:47 +0800

    第一个任务

---
+++ src/main/java/com/qq/demo/Demo.java
@@ -0,0 +1,12 @@
+package com.qq.demo;
+
+/**
+ * @Desc
+ * @Author qiwei.lu@b-and-qchina.com
+ * @Date 2019/11/26 15:12
+ */
+public class Demo {
+    public static void main(String[] args) {
+        //实现百度搜索   “hello world”功能，并且检查第一条搜索结果标题中，是否匹配到“hello world”
+    }
+}

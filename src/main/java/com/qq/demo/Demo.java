689eca4030a7376cd90667dd71cb50d42a2b3d7f
鲁奇伟 <qiwei.lu@b-and-qchina.com>
2019-11-29 17:01:04 +0800

    实现筛选后搜索结果验证功能

--- src/main/java/com/qq/demo/Demo.java
+++ src/main/java/com/qq/demo/Demo.java
@@ -1,5 +1,6 @@
 package com.qq.demo;
 
+import com.qq.util.DateUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -30,7 +31,7 @@ public class Demo {
         driver.manage().window().maximize();
 
         //搜索hello world
-        String keyword="hello world";
+        String keyword = "hello world";
         driver.get("https://www.baidu.com/");
         driver.findElement(By.id("kw")).sendKeys(keyword);
         driver.findElement(By.id("su")).click();
@@ -47,11 +48,36 @@ public class Demo {
             System.out.println("搜索结果不符合预期");
         }
 
+        /**
+         * 搜索筛选实现
+         */
+        //点击"搜索工具"
         driver.findElement(By.className("search_tool")).click();
-
+        //点击"时间不限"
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//*[@class='search_tool_tf ']")).click();
+        Thread.sleep(500);
+        WebElement startDateEle = driver.findElement(By.xpath("//*[@class=\"c-tip-custom-st\"]/input"));
+        WebElement endDateEle = driver.findElement(By.xpath("//*[@class=\"c-tip-custom-et\"]/input"));
+        //输入开始时间与结束时间
+        startDateEle.clear();
+        startDateEle.sendKeys(DateUtil.firstDayOfYear());
+        endDateEle.clear();
+        endDateEle.sendKeys(DateUtil.currentDay());
+        //点击"确认"
+        driver.findElement(By.linkText("确认")).click();
 
-
+        //检查预期
+        Thread.sleep(1000 * 3);//等待搜索结果
+        //搜索结果
+        WebElement h3ByFilter = driver.findElement(By.xpath("//*[@id='content_left']//h3"));//第一行搜索结果的标题
+        //检查标题内容
+        String expectedFilter = h3ByFilter.getText();
+        if (expectedFilter.contains(keyword)) {
+            System.out.println("筛选后搜索正确");
+        } else {
+            System.out.println("筛选后搜索结果不正确");
+        }
 
         //关闭浏览器连接，释放资源
         driver.quit();

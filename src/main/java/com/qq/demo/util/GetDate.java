813e7010875e724b095f720e0c42a00362984858
zhangqingqing <755954406@qq.com>
2019-11-29 14:24:39 +0800

    挪包位置

--- src/main/java/com/qq/demo/util/GetDate.java
+++ src/main/java/com/qq/demo/util/GetDate.java
@@ -1,10 +1,8 @@
-package util;
+package com.qq.demo.util;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Calendar;
 import java.util.Date;
 
 public class GetDate {

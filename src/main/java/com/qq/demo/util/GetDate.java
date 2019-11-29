cfd4336e1dd04128ed6dda4cc63f235c28cde048
zhangqingqing <755954406@qq.com>
2019-11-29 15:07:48 +0800

    小修改

--- src/main/java/com/qq/demo/util/GetDate.java
+++ src/main/java/com/qq/demo/util/GetDate.java
@@ -3,6 +3,7 @@ package com.qq.demo.util;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class GetDate {
@@ -11,7 +12,9 @@ public class GetDate {
     static DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     public static String getDateStart() throws ParseException {
-        return new SimpleDateFormat("yyyy-MM-dd ").format(dateFormat.parse("2019-01-01"));
+        Calendar cr=Calendar.getInstance();
+        cr.set(cr.getWeekYear(),00,01);
+        return new SimpleDateFormat("yyyy-MM-dd ").format(cr.getTime());
     }
 
     public static void setDateStart(String dateStart) {

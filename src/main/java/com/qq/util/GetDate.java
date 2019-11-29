5c00fa298e9ee3615447248919079441a2fb180e
鲁奇伟 <qiwei.lu@b-and-qchina.com>
2019-11-29 16:44:53 +0800

    优化清清的代码

--- src/main/java/com/qq/util/GetDate.java
+++ src/main/java/com/qq/util/GetDate.java
@@ -1,24 +1,21 @@
 package com.qq.util;
 
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class GetDate {
+    //注释无用的代码
+    /*
     private static String dateStart;
-    private static String dateEnd;
+    private static String dateEnd;*/
 
     public static String getDateStart() throws ParseException {
-        Calendar cr=Calendar.getInstance();
-        cr.set(cr.getWeekYear(),00,01);
-        return new SimpleDateFormat("yyyy-MM-dd ").format(cr.getTime());
+        return Calendar.getInstance().get(Calendar.YEAR) + "-01-01";
     }
 
     public static String getDateEnd() {
         return new SimpleDateFormat("yyyy-MM-dd ").format(new Date());
     }
-
-
 }

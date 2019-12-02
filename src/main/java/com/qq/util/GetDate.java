69dd87a629f000e8adcbbca5fd7f01ee4bd6bc53
zhangqingqing <755954406@qq.com>
2019-12-02 14:25:32 +0800

    星期天作用

--- src/main/java/com/qq/util/GetDate.java
+++ src/main/java/com/qq/util/GetDate.java
@@ -18,4 +18,39 @@ public class GetDate {
     public static String getDateEnd() {
         return new SimpleDateFormat("yyyy-MM-dd ").format(new Date());
     }
+
+    public static Long getMonthBegin(String datestr) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(datestr);
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        //将小时至0
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        //将分钟至0
+        c.set(Calendar.MINUTE, 0);
+        //将秒至0
+        c.set(Calendar.SECOND, 0);
+        //将毫秒至0
+        c.set(Calendar.MILLISECOND, 0);
+        // 获取本月第一天的时间戳
+        return c.getTimeInMillis();
+    }
+
+
+    public static Long getMonthEnd(String datestrr) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse(datestrr);
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        //将小时至23
+        c.set(Calendar.HOUR_OF_DAY, 23);
+        //将分钟至59
+        c.set(Calendar.MINUTE, 59);
+        //将秒至59
+        c.set(Calendar.SECOND, 59);
+        //将毫秒至999
+        c.set(Calendar.MILLISECOND, 999);
+        // 获取本月最后一天的时间戳
+        return c.getTimeInMillis();
+    }
 }

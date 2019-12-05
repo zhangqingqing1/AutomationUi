a19c99266d129fbb0ae7ec20b646b261b68e3f9b
鲁奇伟 <qiwei.lu@b-and-qchina.com>
2019-12-05 14:05:56 +0800

    day start and day end

--- src/main/java/com/qq/util/DateUtil.java
+++ src/main/java/com/qq/util/DateUtil.java
@@ -1,6 +1,8 @@
 package com.qq.util;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
@@ -20,5 +22,45 @@ public class DateUtil {
      */
     public static String currentDay() {
         return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+    }
+
+    /**
+     * 获取当天的00:00:00.000
+     *
+     * @param date
+     * @return
+     */
+    public static long dayStart(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        setDayStart(calendar);
+        return calendar.getTimeInMillis();
+    }
+
+    /**
+     * 获取当天的23:59:59.999
+     *
+     * @param date
+     * @return
+     */
+    public static long dayEnd(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        setDayEnd(calendar);
+        return calendar.getTimeInMillis();
+    }
+
+    public static void setDayStart(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+
+    public static void setDayEnd(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
     }
 }

da1d2ea512f3ce35144aa363178153c8af5a00c1
jianhunvxia <zzdxwxj@163.com>
2019-12-10 16:28:47 +0800

    读取excel中的数据

---
+++ src/main/java/com/qq/util/ExcelUtil_wxj.java
@@ -0,0 +1,27 @@
+package com.qq.util;
+
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+
+public class ExcelUtil_wxj {
+    public static void main(String[] args) {
+        try{
+            Workbook workbook = WorkbookFactory.create(new File("C:\\Users\\王香姣\\Desktop\\Excel用例模板.xlsx"));
+            @SuppressWarnings("resource")
+            //加载sheet
+            Sheet sh1 = workbook.getSheetAt(0);
+
+            for(int i=1;i<sh1.getPhysicalNumberOfRows();i++){
+                for (int j=1;j<sh1.getRow(i).getPhysicalNumberOfCells();j++){
+                    System.out.print(sh1.getRow(i).getCell(j).getStringCellValue()+"\t");
+                }
+                System.out.println();
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}

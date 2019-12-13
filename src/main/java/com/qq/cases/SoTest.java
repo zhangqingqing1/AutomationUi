eddb00cb5c9c572cded8bd9ce11c05fd1495009c
鲁奇伟 <qiwei.lu@b-and-qchina.com>
2019-12-13 23:16:44 +0800

    实现360搜索

--- src/main/java/com/qq/cases/SoTest.java
+++ src/main/java/com/qq/cases/SoTest.java
@@ -1,15 +1,17 @@
 package com.qq.cases;
 
-import com.qq.util.DataProvider_zqq;
 import com.qq.util.MyUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import java.io.IOException;
-import java.text.ParseException;
+import java.util.Map;
 
 
 /**
@@ -18,43 +20,36 @@ import java.text.ParseException;
  * @date 2019/12/12 11:01
  */
 public class SoTest {
-        private static WebDriver driver;
-        WebDriverWait wait;
+    private static WebDriver driver;
+    WebDriverWait wait;
 
-        @BeforeClass
-        public void beforeClass() throws  IOException{
-            //定义driver
-            driver = MyUtil.getDriver();
-            driver.manage().window().maximize();
-            wait = new WebDriverWait(driver, 10);//全局设置显示等待10s,超时则异常
-        }
-
-      @Test(dataProvider = "iterator",dataProviderClass = DataProvider_zqq.class)
-      public void searchTest(String keyword, String startTime, String endTime) {
-            //搜索hello world
-            driver.get("https://www.baidu.com/");
-            driver.findElement(By.id("kw")).sendKeys(keyword);
-            driver.findElement(By.id("su")).click();
-
-            //搜索结果
-            WebElement h3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='content_left']//h3")));
-            //检查标题内容
-            String expected = h3.getText();
-            if (expected.contains(keyword)) {
-                System.out.println("搜索成功，符合预期");
-            } else {
-                System.out.println("搜索结果不符合预期");
-            }
-        }
-
-
-
-        @AfterClass
-        public void AfterTest() {
-            driver.quit();
-        }
-
+    @BeforeClass
+    public void beforeClass() throws IOException {
+        //定义driver
+        driver = MyUtil.getDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);//全局设置显示等待10s,超时则异常
     }
+
+    @Test(dataProviderClass = com.qq.util.QQDataProvider.class, dataProvider = "provider")
+    public void searchTest(Map<String, String> map) {
+        String keyword = map.get("keyword");
+        driver.get("https://www.so.com");
+        WebElement input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input")));
+        input.clear();
+        input.sendKeys(keyword);
+        driver.findElement(By.id("search-button")).click();
+
+        WebElement result = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='result']//h3")));
+
+        if (!result.getText().contains(keyword)) throw new AssertionError("360搜索结果不符合预期");
+    }
+
+    @AfterClass
+    public void AfterTest() {
+        driver.quit();
+    }
+}
 
 
 

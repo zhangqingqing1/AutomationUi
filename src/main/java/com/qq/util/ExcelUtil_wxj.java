8e2691084c69357b2d7a9918773b93ec280b6b5c
jianhunvxia <zzdxwxj@163.com>
2019-12-17 16:41:25 +0800

    优化

--- src/main/java/com/qq/util/ExcelUtil_wxj.java
+++ src/main/java/com/qq/util/ExcelUtil_wxj.java
@@ -9,24 +9,25 @@ import java.util.HashMap;
 import java.util.List;
 
 public class ExcelUtil_wxj {
-    public static List<HashMap<String, Object>> readExcel(File file) throws IOException {
+    public static List<HashMap<String, String>> readExcel(File file,String sheetName) throws IOException {
         // 创建一个list 用来存储读取的内容
-        List<HashMap<String, Object>> dataList = new ArrayList<>();
+        List<HashMap<String, String>> dataList = new ArrayList<>();
         Workbook workbook = WorkbookFactory.create(file);
         @SuppressWarnings("resource")
         //加载sheet
-        Sheet sh1 = workbook.getSheetAt(0);
-        for (int i=1;i<sh1.getPhysicalNumberOfRows(); i++) {
-            HashMap<String, Object> line = new HashMap<String, Object>();
+        Sheet sh1 = workbook.getSheet(sheetName);
+        for (int rowNum=1;rowNum<sh1.getPhysicalNumberOfRows(); rowNum++) {
+            HashMap<String, String> line = new HashMap<>();
             // 创建一个数组用来存储每一列的值
-            String[] str = new String[sh1.getRow(i).getPhysicalNumberOfCells()];
-            // 列数
-            for (int j=1;j<sh1.getRow(i).getPhysicalNumberOfCells(); j++) {
-                // 获取第i行，第j列的值
-                str[j] = sh1.getRow(i).getCell(j).getStringCellValue();
-                line.put(sh1.getRow(0).getCell(j).getStringCellValue(), str[j]);
+            String[] str = new String[sh1.getRow(rowNum).getPhysicalNumberOfCells()];
+            for (int cellNum=1;cellNum<sh1.getRow(rowNum).getPhysicalNumberOfCells(); cellNum++) {
+                str[cellNum] = sh1.getRow(rowNum).getCell(cellNum).getStringCellValue();
+                line.put(sh1.getRow(0).getCell(cellNum).getStringCellValue(), str[cellNum]);
             }
             dataList.add(line);
+        }
+        if(null != workbook){
+            workbook.close();
         }
         return dataList;
     }

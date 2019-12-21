732160ce2806a2a1a9609521b61e718927f9f6d4
zhangqingqing <755954406@qq.com>
2019-12-21 16:59:24 +0800

    Merge branch 'dev' into zqq
    Signed-off-by: zhangqingqing <755954406@qq.com>
    
    # Conflicts:
    #	src/main/resources/testng.xml

--- src/main/java/com/qq/cases/SoTest.java
+++ src/main/java/com/qq/cases/SoTest.java
@@ -6,9 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -19,21 +17,22 @@ import java.util.Map;
  * @author: lu
  * @date 2019/12/12 11:01
  */
-public class SoTest {
-    private static WebDriver driver;
-    WebDriverWait wait;
-
-    @BeforeClass
-    public void beforeClass() throws IOException {
-        //定义driver
-        driver = MyUtil.getDriver();
-        driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, 10);//全局设置显示等待10s,超时则异常
-    }
+public class SoTest extends  BaseTest{
+//    private static WebDriver driver;
+//    WebDriverWait wait;
+//
+//    @BeforeTest
+//    public void beforeClass() throws IOException {
+//        //定义driver
+//        driver = MyUtil.getDriver();
+//        driver.manage().window().maximize();
+//        wait = new WebDriverWait(driver, 10);//全局设置显示等待10s,超时则异常
+//    }
 
     @Test(dataProviderClass = com.qq.util.QQDataProvider.class, dataProvider = "provider")
-    public void searchTest(Map<String, String> map) {
+    public void searchTest(Map<String, String> map) throws InterruptedException {
         String keyword = map.get("keyword");
+        Thread.sleep(1000*2);
         driver.get("https://www.so.com");
         WebElement input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input")));
         input.clear();
@@ -44,11 +43,11 @@ public class SoTest {
 
         if (!result.getText().contains(keyword)) throw new AssertionError("360搜索结果不符合预期");
     }
-
-    @AfterClass
-    public void AfterTest() {
-        driver.quit();
-    }
+//
+//    @AfterTest
+//    public void AfterTest() {
+//        driver.quit();
+//    }
 }
 
 

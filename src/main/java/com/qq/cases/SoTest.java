d8bb88aa9cc3c2bd5f5ab74be027e04a82766f55
zhangqingqing <755954406@qq.com>
2019-12-24 11:00:56 +0800

    修改
    Signed-off-by: zhangqingqing <755954406@qq.com>

--- src/main/java/com/qq/cases/SoTest.java
+++ src/main/java/com/qq/cases/SoTest.java
@@ -3,8 +3,10 @@ package com.qq.cases;
 import com.qq.Factory.WebDriverFactory;
 import com.qq.pages.SoPage;
 import com.qq.util.QQDataProvider;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -23,7 +25,7 @@ import static com.qq.pages.BaiduPage.*;
 public class SoTest {
     private WebDriver driver;
     private WebDriverWait wait;
-    SoPage soPage=new  SoPage();
+    SoPage soPage = new SoPage();
 
     @BeforeClass
     public void beforeClass() throws IOException {
@@ -31,21 +33,19 @@ public class SoTest {
         wait = new WebDriverWait(driver, 10);
     }
 
-    @Test(dataProviderClass = QQDataProvider.class, dataProvider = "provider")
+    @Test(dataProviderClass = com.qq.util.QQDataProvider.class, dataProvider = "provider")
     public void searchTest(Map<String, String> map) {
         String keyword = map.get("keyword");
         driver.get("https://www.so.com");
-        WebElement input = soPage.waitWebElementByid("input");
+        WebElement input = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("input")));
         input.clear();
         input.sendKeys(keyword);
-        soPage.searchButtonClick();
+        driver.findElement(By.id("search-button")).click();
 
-        WebElement result =soPage.waitWebElementByxpath("//*[@class='result']//h3");
+        WebElement result = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@class='result']//h3")));
 
         if (!result.getText().contains(keyword)) throw new AssertionError("360搜索结果不符合预期");
     }
+
 }
-
-
-
 
